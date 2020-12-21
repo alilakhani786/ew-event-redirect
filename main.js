7f9987b0f547aa4eeda8f510acd659bc1cf95ca1
Ali Lakhani <nlakhani@akamai.com>
2020-12-21 13:30:13 +0800

    Showtime - v1.6

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Ali Redirect Demo
 
 export function onClientRequest(request) {
-	request.respondWith(302, {'Location': ['http://www.singtel.com']}, '');
+	request.respondWith(302, {'Location': ['http://www.starhub.com.com']}, '');
 }
 
 
